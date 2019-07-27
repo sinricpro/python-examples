c8fbff0ffb0279a4b1b34ef5a3ab6ee275bc01f1
Dhanush <dhanushdazz@gmail.com>
2019-07-27 14:34:59 +0530

    Changed way of calling events

--- pro_doorbell_example/app.py
+++ pro_doorbell_example/app.py
@@ -3,25 +3,24 @@ from credentials import apiKey, deviceId, doorBellId
 from sinric import SinricProUdp
 from sinric import eventNames
 from time import sleep
-import sys
 
 '''
 DON'T FORGET TO TURN ON 'Doorbell Press' IN ALEXA APP
 '''
 
 
-def door_bell_event():
-    # while True:
-    client.event_handler.raiseEvent(doorBellId, eventNames['door_bell_event'])
-    sleep(1)
-    sys.exit(0)
+def Events():
+    while True:
+        client.event_handler.raiseEvent(doorBellId, eventNames['door_bell_event'])
+        sleep(2)
 
 
 request_callbacks = {}
 
 event_callbacks = {
-    'door_bell_event': door_bell_event
+    'Events': Events
 }
+
 if __name__ == '__main__':
     client = SinricPro(apiKey, deviceId, request_callbacks, event_callbacks, enable_trace=True)
     udp_client = SinricProUdp(request_callbacks)
